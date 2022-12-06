2c4e9e5ad4f344ee9b8438994d802e4f451749aa
Mike O'Connor <mike_oconnor@outlook.com>
2022-12-06 12:42:13 -0600

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> caloriesPerElf = new List<int>();
+
+            int runningTotal = 0;
+            var lines = File.ReadAllLines("Input.txt");
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    caloriesPerElf.Add(runningTotal);
+                    runningTotal = 0;
+                }
+                else 
+                {
+                    runningTotal += int.Parse(line);
+                }
+            }
+
+            Console.WriteLine("Max for one elf: {0}", caloriesPerElf.Max());
+            Console.WriteLine("Max for three elves: {0}", caloriesPerElf.OrderByDescending(c => c).Take(3).Sum());
+        }
+    }
+}
